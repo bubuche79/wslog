2632381107141082ff5decfd38f90cd5ddc9198c
gbuchetet <gaetan.buchetet@squoring.com>
2017-01-04 14:12:08 +0100

    WIP: daemon startup
    - drop privileges
    - create pid file

---
+++ src/cmd/wslogd/wslogd.h
@@ -0,0 +1,16 @@
+#ifndef _WSLOGD_H
+#define _WSLOGD_H
+
+#include "conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const struct ws_conf *confp;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _DAEMON_H */
