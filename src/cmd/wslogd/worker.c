14db4cc8fcb2bd3b4c897be7964d3faa4a6cbe2a
gbuchetet <gaetan.buchetet@squoring.com>
2017-01-11 10:32:00 +0100

    WIP: daemon startup (fork mode)

--- src/cmd/wslogd/worker.c
+++ src/cmd/wslogd/worker.c
@@ -7,6 +7,11 @@
 #include <errno.h>
 #include <syslog.h>
 #include <time.h>
+#ifdef _USE_FORK
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#endif
 
 #include "defs/std.h"
 
@@ -19,14 +24,19 @@
 #include "worker.h"
 
 struct worker {
+	const char *w_name;								/* worker name */
 	int w_signo;									/* signal number */
 	struct timespec w_ifreq;						/* timer interval */
 	int (*w_init) (void);
 	int (*w_action) (void);							/* action on signal */
 	int (*w_destroy) (void);
 
-	timer_t dt_timer;
-	pthread_t dt_thread;
+	timer_t w_timer;
+#ifdef _USE_FORK
+	pid_t w_pid;									/* child pid */
+#else
+	pthread_t w_thread;								/* thread id */
+#endif
 };
 
 static int startup = 1;
@@ -37,16 +47,25 @@ static volatile sig_atomic_t hangup_pending;
 static struct worker threads[3];				/* daemon threads */
 static size_t threads_nel;							/* number of elements */
 
-static int
-sig_block(void)
+static void
+sig_set(sigset_t *set)
 {
-	sigset_t set;
+	(void) sigemptyset(set);
+	(void) sigaddset(set, SIGHUP);
+	(void) sigaddset(set, SIGTERM);
+#ifdef _USE_FORK
+	(void) sigaddset(set, SIGCHLD);
+#endif
+}
 
-	(void) sigemptyset(&set);
-	(void) sigaddset(&set, SIGHUP);
-	(void) sigaddset(&set, SIGTERM);
-
-	return pthread_sigmask(SIG_BLOCK, &set, NULL);
+static void
+sigthread_exit(int status)
+{
+#ifdef _USE_FORK
+	exit(status);
+#else
+	pthread_exit(NULL);
+#endif
 }
 
 static void *
@@ -73,12 +92,14 @@ sigthread_main(void *arg)
 	/* Change blocked signals */
 	(void) sigemptyset(&set);
 	(void) sigaddset(&set, dt->w_signo);
-//	(void) pthread_sigmask(SIG_BLOCK, &set, &oset);
+#ifdef _USE_FORK
+	(void) pthread_sigmask(SIG_SETMASK, &set, NULL);
+#endif
 
 	/* Create timer */
 	se.sigev_notify = SIGEV_SIGNAL;
 	se.sigev_signo = dt->w_signo;
-	se.sigev_value.sival_ptr = &dt->dt_timer;
+	se.sigev_value.sival_ptr = &dt->w_timer;
 
 	itimer.it_interval.tv_sec = dt->w_ifreq.tv_sec;
 	itimer.it_interval.tv_nsec = dt->w_ifreq.tv_nsec;
@@ -126,6 +147,8 @@ sigthread_main(void *arg)
 		}
 	}
 
+	sigthread_exit(0);
+
 	return NULL;
 
 error:
@@ -134,16 +157,32 @@ error:
 	(void) dt->w_destroy();
 
 	errno = errsv;
+	sigthread_exit(1);
 	return NULL;
 }
 
 static int
 sigthread_create(struct worker *dt)
 {
-	if (pthread_create(&dt->dt_thread, NULL, sigthread_main, dt) == -1) {
+#ifdef _USE_FORK
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1) {
+		syslog(LOG_ALERT, "fork: %m");
+		return -1;
+	} else if (pid > 0) {
+		dt->w_pid = pid;
+	} else {
+		(void) sigthread_main(dt);
+	}
+
+#else
+	if (pthread_create(&dt->w_thread, NULL, sigthread_main, dt) == -1) {
 		syslog(LOG_EMERG, "pthread_create(): %m");
 		return -1;
 	}
+#endif /* _USE_FORK */
 
 	return 0;
 }
@@ -152,17 +191,30 @@ static int
 sigthread_kill(struct worker *dt)
 {
 	int ret;
+#ifdef _USE_FORK
+	siginfo_t info;
+#else
 	sigset_t set;
+#endif
 
 	ret = 0;
 
+#ifdef _USE_FORK
+	if (kill(dt->w_pid, SIGTERM) == -1) {
+		ret = -1;
+	} else {
+		if (waitid(P_PID, dt->w_pid, &info, 0) == -1) {
+			ret = -1;
+		}
+	}
+#else
 	/* Kill thread */
-	if (pthread_kill(dt->dt_thread, dt->w_signo) == -1) {
+	if (pthread_kill(dt->w_thread, dt->w_signo) == -1) {
 		ret = -1;
 	} else {
 		void *th_res;
 
-		if (pthread_join(dt->dt_thread, &th_res) == -1) {
+		if (pthread_join(dt->w_thread, &th_res) == -1) {
 			ret = -1;
 		}
 	}
@@ -174,6 +226,7 @@ sigthread_kill(struct worker *dt)
 	if (pthread_sigmask(SIG_UNBLOCK, &set, NULL) == -1) {
 		return -1;
 	}
+#endif /* _USE_FORK */
 
 	return ret;
 }
@@ -196,12 +249,18 @@ threads_start(void)
 {
 	int signo;
 	size_t i = 0;
+#ifndef _USE_FORK
 	sigset_t set;
+#endif
 
 	threads_count();
 
 	/* Signal */
+#ifdef _USE_FORK
+	signo = SIGALRM;
+#else
 	signo = SIGRTMIN;
+#endif
 
 	/* Configure reader thread */
 	threads[i].w_signo = signo;
@@ -212,7 +271,9 @@ threads_start(void)
 	threads[i].w_destroy = ws23xx_destroy;
 
 	i++;
+#ifndef _USE_FORK
 	signo++;
+#endif
 
 	/* Configure CSV thread */
 	if (!confp->csv.disabled) {
@@ -224,7 +285,9 @@ threads_start(void)
 		threads[i].w_destroy = csv_destroy;
 
 		i++;
+#ifndef _USE_FORK
 		signo++;
+#endif
 	}
 
 	/* Configure Wunder thread */
@@ -237,10 +300,13 @@ threads_start(void)
 		threads[i].w_destroy = wunder_destroy;
 
 		i++;
+#ifndef _USE_FORK
 		signo++;
+#endif
 	}
 
 	/* Manage signals */
+#ifndef _USE_FORK
 	(void) sigemptyset(&set);
 
 	for (i = 0; i < threads_nel; i++) {
@@ -250,6 +316,7 @@ threads_start(void)
 	if (pthread_sigmask(SIG_BLOCK, &set, NULL) == -1) {
 		return -1;
 	}
+#endif
 
 	/* Create threads */
 	for (i = 0; i < threads_nel; i++) {
@@ -308,14 +375,17 @@ int
 worker_main(int *halt)
 {
 	int errsv;
+	sigset_t set;
 
 	shutdown_pending = 0;
 	hangup_pending = 0;
 
+	sig_set(&set);
+
 	/* Startup initialization */
 	if (startup) {
-		if (sig_block() == -1) {
-			syslog(LOG_ERR, "set_signals: %m");
+		if (pthread_sigmask(SIG_BLOCK, &set, NULL) == -1) {
+			syslog(LOG_ERR, "pthread_sigmask: %m");
 			return -1;
 		}
 
@@ -328,7 +398,7 @@ worker_main(int *halt)
 		startup = 0;
 	}
 
-	/* Spawn all threads */
+	/* Start all workers */
 	if (threads_start() == -1) {
 		syslog(LOG_EMERG, "threads_start(): %m");
 		goto error;
@@ -337,14 +407,9 @@ worker_main(int *halt)
 	/* Wait for events */
 	while (!shutdown_pending && !hangup_pending) {
 		int ret;
-		sigset_t set;
 		siginfo_t info;
 
 		/* Signals to wait for */
-		(void) sigemptyset(&set);
-		(void) sigaddset(&set, SIGHUP);
-		(void) sigaddset(&set, SIGTERM);
-
 		ret = sigwaitinfo(&set, &info);
 		if (ret == -1) {
 			syslog(LOG_ERR, "sigwaitinfo(): %m");
@@ -358,6 +423,11 @@ worker_main(int *halt)
 				shutdown_pending = 1;
 				syslog(LOG_NOTICE, "Signal TERM");
 				break;
+#ifdef _USE_FORK
+			case SIGCHLD:
+				syslog(LOG_ERR, "Signal CHLD");
+				break;
+#endif
 			default:
 				syslog(LOG_ERR, "Signal %d", ret);
 				break;
