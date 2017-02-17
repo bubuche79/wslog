d21022660b1cc87df8d0477161e4b8c10ec32378
gbuchetet <gaetan.buchetet@squoring.com>
2017-02-17 22:58:54 +0100

    Add Lua C module (wsview.so)
    The purpose of this module is to access to the shared board from the CGI
    module.

---
+++ src/wslogd/wsview.c
@@ -0,0 +1,106 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string.h>
+#include <errno.h>
+
+#include "defs/dso.h"
+#include "defs/std.h"
+
+#include "board.h"
+#include "dataset.h"
+#include "wsview.h"
+
+struct lua_table
+{
+	const char *name;
+	int (*get) (const struct ws_loop *, double *);
+};
+
+static int
+ws_current(lua_State *L)
+{
+	int res;
+	size_t i, nel;
+	struct ws_loop buf;
+	struct ws_loop *bufp;
+
+	struct lua_table fields[] =
+	{
+		{ "pressure", ws_get_pressure },
+		{ "barometer", ws_get_barometer },
+		{ "temp", ws_get_temp },
+		{ "humidity", ws_get_humidity },
+		{ "wind_speed", ws_get_wind_speed },
+		{ "wind_dir", ws_get_wind_dir },
+		{ "wind_gust_speed", ws_get_wind_gust_speed },
+		{ "wind_gust_dir", ws_get_wind_gust_dir },
+		{ "rain", ws_get_rain },
+		{ "rain_rate", ws_get_rain_rate },
+		{ "dew_point", ws_get_dew_point },
+		{ "windchill", ws_get_windchill },
+		{ "heat_index", ws_get_heat_index },
+		{ "temp_in", ws_get_temp_in },
+		{ "humidity_in", ws_get_humidity_in }
+	};
+
+	/* Read shared board */
+	if (board_lock() == -1) {
+		goto error;
+	}
+
+	bufp = board_peek(0);
+
+	if (bufp != NULL) {
+		memcpy(&buf, bufp, sizeof(buf));
+	}
+
+	if (board_unlock() == -1) {
+		goto error;
+	}
+
+	/* Lua result */
+	if (bufp == NULL) {
+		res = 0;
+	} else {
+		res = 1;
+
+		lua_newtable(L);
+
+		/* Add fields to table */
+		nel = array_size(fields);
+
+		for (i = 0; i < nel; i++) {
+			double value;
+
+			if (fields[i].get(bufp, &value) == 0) {
+				lua_pushnumber(L, value);
+				lua_setfield(L, -2, fields[i].name);
+			}
+		}
+
+		/* Add time */
+		lua_pushinteger(L, bufp->time);
+		lua_setfield(L, -2, "time");
+	}
+
+	return res;
+
+error:
+	lua_pushfstring(L, "board_unlock: %s", strerror(errno));
+	return lua_error(L);
+}
+
+DSO_EXPORT int
+luaopen_wsview(lua_State *L)
+{
+	lua_register(L, "ws_current", ws_current);
+
+	if (board_open(0) == -1) {
+        lua_pushfstring(L, "board_open: %s", strerror(errno));
+		return lua_error(L);
+	}
+
+    return 0;
+}
---
+++ src/wslogd/wsview.h
@@ -0,0 +1,16 @@
+#ifndef _WSVIEW_H
+#define _WSVIEW_H
+
+#include <lua.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int luaopen_wsview(lua_State *l);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WSVIEW_H */
