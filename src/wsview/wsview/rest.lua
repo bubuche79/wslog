ca9f84e800eaa62170e88d51253b86c21a685803
gbuchetet <gaetan.buchetet@squoring.com>
2017-03-11 21:07:45 +0100

    Add climate by-year rest api

--- src/wsview/wsview/rest.lua
+++ src/wsview/wsview/rest.lua
@@ -1,7 +1,7 @@
 local coroutine = require "coroutine"
 local http = require "wsview.http"
 
-function sql_query(s, e)
+function sql_month(s, e)
 	local sql = "SELECT date(time, 'unixepoch') AS day, "
 	sql = sql .. "MIN(temp) AS temp_min, "
 	sql = sql .. "MAX(temp) AS temp_max, "
@@ -17,15 +17,36 @@ function sql_query(s, e)
 	return cnx:execute(sql)
 end
 
+function sql_year(s, e)
+	local sql = "SELECT strftime('%Y-%m', time, 'unixepoch') AS month, "
+	sql = sql .. "SUM(rain) AS rain "
+	sql = sql .. "FROM ws_archive "
+	sql = sql .. string.format("WHERE %d <= time AND time < %d ", s, e)
+	sql = sql .. "GROUP BY strftime('%Y-%m', time, 'unixepoch') "
+	sql = sql .. "ORDER BY strftime('%Y-%m', time, 'unixepoch') "
+
+	return cnx:execute(sql)
+end
+
 function write_json(n, obj)
 	http.write(string.format('"%s":', n))
 	http.write_json(obj)
 end
 
-function rest_json(s, e)
-	local first = true
+function rest_cb_year(row)
+	local month_num = string.gsub(row.month, '.*-', '')
 
-	local cur = sql_query(s, e)
+	row.month = tonumber(month_num)
+end
+
+function rest_cb_month(row)
+	local day_num = string.gsub(row.day, '.*-', '')
+
+	row.day = tonumber(day_num)
+end
+
+function rest_json(cur, cb)
+	local first = true
 	local row = cur:fetch({}, "a")
 
 	http.write(',"data":[')
@@ -34,9 +55,7 @@ function rest_json(s, e)
 			http.write(",")
 		end
 
-		local day_num = string.gsub(row.day, '.*-', '')
-
-		row.day = tonumber(day_num)
+		cb(row)
 		http.write_json(row)
 
 		-- next row
@@ -45,20 +64,35 @@ function rest_json(s, e)
 	end
 	http.write("]")
 
-	cur:close()
 end
 
 function aggr_month(y, m)
-	local from = os.time({ year = y, month = m, day = 1 })
-	local to = os.time({ year = y, month = m+1, day = 1 })
+	local from, to
+	local cur
+
+	if (m == nil) then
+		from = os.time({ year = y, month = 1, day = 1 })
+		to = os.time({ year = y+1, month = 1, day = 1 })
+		cur = sql_year(from, to)
+	else
+		from = os.time({ year = y, month = m, day = 1 })
+		to = os.time({ year = y, month = m+1, day = 1 })
+		cur = sql_month(from, to)
+	end
 
 	http.prepare_content("application/json; charset=utf-8")
-
 	http.write('{')
-	write_json('period', { year = y, month = m })
 
-	rest_json(from, to)
+	if (m == nil) then
+		write_json('period', { year = y })
+		rest_json(cur, rest_cb_year)
+	else
+		write_json('period', { year = y, month = m })
+		rest_json(cur, rest_cb_month)
+	end
 
 	http.write('}')
 	http.close()
+
+	cur:close()
 end
